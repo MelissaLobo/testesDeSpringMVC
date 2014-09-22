6a073e157076bdb1c56d04083190ce4829701e02
Melissa Lobo <mellobomel@gmail.com>
2014-09-22 01:33:49 -0300

    Removendo

--- src/main/java/util/EntityLocal.java
+++
@@ -1,9 +0,0 @@
-package util;
-
-import javax.persistence.EntityManager;
-
-public class EntityLocal {
-
-	public static ThreadLocal<EntityManager> currentEntityManager = new ThreadLocal<EntityManager>();
-
-}
